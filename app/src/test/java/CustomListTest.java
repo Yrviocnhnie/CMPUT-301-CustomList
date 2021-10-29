03b513f0f7920047cb21386e6169015e535c1aca
Yrviocnhnie <ruiqing2@ualberta.ca>
2021-10-29 13:36:21 -0600

    Delete_failure

--- app/src/test/java/CustomListTest.java
+++ app/src/test/java/CustomListTest.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.example.simpleparadox.listycity.City;
@@ -18,20 +19,30 @@ public class CustomListTest {
         list = new CustomList(null, new ArrayList<City>());
     }
 
-//    @Test
-//    public void addCityTest(){
-//        City c = new City("Edmonton", "AB");
-//
-//        int size = list.getCount();
-//        list.addCity(c);
-//        assertEquals(list.getCount(),size+1);
-//    }
+    @Test
+    public void addCityTest(){
+        City c = new City("Edmonton", "AB");
+
+        int size = list.getCount();
+        list.addCity(c);
+        assertEquals(list.getCount(),size+1);
+    }
 
     @Test
     public void hasCityTest(){
         City c = new City("Edmonton", "AB");
         list.addCity(c);
         assertTrue(list.hasCity(c));
+    }
+
+    @Test
+    public void testDelete(){
+        City c1 = new City("Edmonton", "AB");
+        list.addCity(c1);
+
+        list.delete(c1);
+        assertFalse(list.hasCity(c1));
+
     }
 
 }
